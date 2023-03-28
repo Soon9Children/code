49a7732376f9be139ac7435ff60432d60ed97c01
Sumin Park <126743151+330sum@users.noreply.github.com>
2023-03-28 18:26:08 +0900

    집사야 밥줘 데모!

---
+++ three.js
@@ -0,0 +1,276 @@
+// DOM
+const $background = document.querySelector('.background > ul');
+// console.log(background);
+const $cat = document.getElementById('cat');
+// console.log($cat);
+const $fishes = document.querySelector('.fishes');
+// console.log($fishes);
+$fishChildren = [...$fishes.children];
+
+
+// Setting
+const GAME_COLS = 20; // 가로
+const GAME_ROWS = 9; // 세로
+
+let score = 0;
+let duration = 500;
+let tempMovingCat;
+let tempMovingFish;
+let x=0;
+let y=0;
+
+
+const BLOCKS = {
+    cat: [
+        [
+            [8, 5],
+        ]
+    ],
+    fish: [
+        [
+            [0, 5],
+        ]
+    ]
+}
+
+// console.log(BLOCKS.fish[0][0].pop()      );
+
+// if (x===BLOCKS.fish[0][0][0] && y===BLOCKS.fish[0][0][1] ) {
+//     BLOCKS.fish[0][0].pop();
+// }
+
+console.log(BLOCKS.fish);
+
+const movingCat = {
+    name: "냥냥이",
+    type: 'cat',
+    direction: 0,
+    left: 0,
+    top: 0
+}
+
+const movingFish = {
+    name: "물고기",
+    type: 'fish',
+    direction: 0,
+    left: 0,
+    top: 0,
+}
+
+
+// 게임시작
+function init() {
+    tempMovingCat = {
+        ...movingCat
+    };
+    tempMovingFish = {
+        ...movingFish
+    };
+    for (let i = 0; i < GAME_ROWS; i++) {
+        prependNewLine()
+    }
+    drawCat()
+    drawFish()
+}
+
+function prependNewLine() {
+    const li = document.createElement('li');
+    const ul = document.createElement('ul');
+    for (let j = 0; j < GAME_COLS; j++) {
+        const matrix = document.createElement('li');
+        ul.prepend(matrix);
+    }
+    li.prepend(ul);
+    $background.prepend(li);
+}
+
+function drawCat() {
+    const {
+        type,
+        direction,
+        left,
+        top
+    } = tempMovingCat;
+    const movingBlocks = document.querySelectorAll(".moving")
+    movingBlocks.forEach(moving => {
+        moving.classList.remove(type, "moving")
+    })
+    BLOCKS[type][direction].forEach(block => {
+        const x = block[0] + left;
+        const y = block[1] + top;
+        // console.log({$background});
+        
+        // 범위 넘어가면 null
+        const target = $background.childNodes[y] ? $background.childNodes[y].childNodes[0].childNodes[x] : null;
+        const isAvailable = checkEmpty(target);
+        if (isAvailable) {
+            target.classList.add(type, "moving");
+        } else {
+            tempMovingCat = {
+                ...movingCat
+            }
+            setTimeout(() => {
+                drawCat();
+            }, 0)
+        }
+        
+      
+        console.log(x,y);
+        console.log(BLOCKS.fish[0][0][0], BLOCKS.fish[0][0][1]);
+        if (x===BLOCKS.fish[0][0][0] && y===BLOCKS.fish[0][0][1] ) {
+            BLOCKS.fish[0][0].pop();
+        }    
+    })
+    movingCat.left = left;
+    movingCat.top = top;
+    
+}
+
+function moveBlock(moveType, amount) {
+    tempMovingCat[moveType] += amount;
+    drawCat()
+}
+
+
+function drawFish() {
+    const {
+        type,
+        direction,
+        left,
+        top,
+    } = tempMovingFish;
+    BLOCKS[type][direction].forEach(block => {
+        x = block[0] + left;
+        y = block[1] + top;
+
+        const target = $background.childNodes[y] ? $background.childNodes[y].childNodes[0].childNodes[x] : null;
+        const isAvailable = checkEmpty(target);
+        if (isAvailable) {
+            target.classList.add(type, "moving");
+        } else {
+            tempMovingFish = {
+                ...movingFish
+            }
+            setTimeout(() => {
+                drawFish();
+            }, 0)
+        }
+    })
+}
+
+
+
+
+function checkEmpty(target) {
+    if (!target) {
+        return false;
+    }
+    return true;
+}
+
+
+
+
+
+
+// 이벤트 핸들링
+document.addEventListener('keydown', e => {
+    switch (e.keyCode) {
+        case 39:
+            moveBlock("left", 1);
+            break;
+        case 37:
+            moveBlock("left", -1);
+            break;
+        case 38:
+            moveBlock("top", -1);
+            break;
+        case 40:
+            moveBlock("top", 1);
+            break;
+        default:
+            break;
+    }
+    // console.log(e);
+})
+
+
+
+
+// 물고기 생성
+// function createFish() {
+//     const ul = document.createElement('ul');
+// }
+
+
+
+
+
+
+
+
+
+
+//랜덤으로 생성하는 함수!
+// const randomCreate = () => {
+//     let n = Math.floor(Math.random() * 18) + 1;
+//     const $layer = document.querySelector(`.layer li:nth-child(${n})`);
+//     const $newLayer = document.createElement(`div`);
+//     // $newLayer.innerHTML = '<img src=../images/fish.png>';
+//     $layer.classList.add(`fall`);
+//     $layer.appendChild($newLayer);
+//     console.log($layer);
+//     return $newLayer;
+
+// };
+
+// for (i = 0; i < 1000; i++) {
+//     setTimeout(() => {
+//         randomCreate();
+//     }, Math.random() * 1000000);
+// }
+
+
+
+
+
+
+
+
+
+
+// function countdown() {
+//     let seconds = 10;
+//     function tick() {
+//         let $counter = document.getElementById('counter');
+//         seconds--;
+//         $counter.innerHTML = (seconds < 10 ? '0' : '')  + String(seconds) + '초';
+//         if( seconds > 0 ) {
+//             setTimeout(tick, 1000);
+//         } else {
+//             alert('집사야~ 잘 먹었다옹~');
+//         }
+//     }
+//     tick();
+// }
+
+// countdown();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 게임시작
+init();
